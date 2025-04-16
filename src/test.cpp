619d878996de9329881a7ec99da039b4b7d5cb43
unalive <nonadhocproblems@gmail.com>
2025-04-16 17:46:28 +0530

    minor changes

---
+++ src/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "dynamic_bitset.hpp"
+
+using dbitset = dybi::dynamic_bitset<uint64_t, 64>;
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    dbitset a(n);
+    
+}
